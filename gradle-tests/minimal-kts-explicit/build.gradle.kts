9711d237bdd3490e98a1be79ba8ec8ffc568909e
Esta Nagy <nagyesta@gmail.com>
2025-04-20 22:24:05 +0200

    Maintenance (#130)
    - Configure Qlty
    - Migrate to Sonar
    - Stop using the third party PR labeler
    - Reconfigure Renovate
    
    {minor}
    
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

--- gradle-tests/minimal-kts-explicit/build.gradle.kts
+++ gradle-tests/minimal-kts-explicit/build.gradle.kts
@@ -7,7 +7,9 @@ group = "com.github.nagyesta.run-dash-license-tool.test"
 version = "0.0.1"
 
 dependencies {
+    @Suppress("kotlin:S6624")
     implementation("org.apache.commons:commons-lang3:3.14.0")
+    @Suppress("kotlin:S6624")
     testImplementation("org.junit.jupiter:junit-jupiter:[5.8.0,5.8.1)")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
--- gradle-tests/minimal-kts/build.gradle.kts
+++ gradle-tests/minimal-kts/build.gradle.kts
@@ -7,7 +7,9 @@ group = "com.github.nagyesta.run-dash-license-tool.test"
 version = "0.0.1"
 
 dependencies {
+    @Suppress("kotlin:S6624")
     implementation("org.apache.commons:commons-lang3:+")
+    @Suppress("kotlin:S6624")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
